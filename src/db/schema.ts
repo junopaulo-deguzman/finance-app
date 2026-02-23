1e151e7217162fcb065e17ac5c4f5bbc4b637adc
Juno <jpaulodeguzman@earnest.com>
2026-02-24 00:05:23 +0800

    Savings schema added

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,9 +6,25 @@ export const transactions = sqliteTable("transactions", {
   category: text("category").notNull(),
   note: text("note").notNull().default(""),
   amount: real("amount").notNull(),
-  type: text("type", { enum: ["income", "expense"] }).notNull(),
+  type: text("type", { enum: ["income", "expense", "save"] }).notNull(),
   createdAt: integer("created_at", { mode: "timestamp_ms" }).notNull(),
 });
+
+export const savings = sqliteTable("savings", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull().default(""),
+  targetAmount: real("target_amount"),
+  targetDate: text("target_date"),
+  createdAt: integer("created_at", { mode: "timestamp_ms" }).notNull(),
+  lastUpdatedAt: integer("last_updated_at", { mode: "timestamp_ms" }).notNull(),
+});
+
+export const savings_allocations = sqliteTable("savings_allocations", {
+  id: text("id").primaryKey(),
+  savingsId: text("savingsId").references(() => savings.id),
+  transactionId: text("transactionId").references(() => transactions.id),
+  createdAt: integer("created_at", { mode: "timestamp_ms" }).notNull(),
+})
 
 export type Transaction = typeof transactions.$inferSelect;
 export type NewTransaction = typeof transactions.$inferInsert;
